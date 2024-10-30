728fc77d999babef46379113fcf1778267713473
shaansundar <shaan.idtindia@gmail.com>
2024-10-30 18:05:36 +0530

    fix: global fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,18 +14,8 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-
       <body className={inter.className}>{children}</body>
     </html>
   );
