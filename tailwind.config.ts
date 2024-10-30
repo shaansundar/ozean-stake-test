a899a00ca4bf3082282ffbd7b0d512e549ab50c9
shaansundar <shaan.idtindia@gmail.com>
2024-10-30 17:39:35 +0530

    feat: theme setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,14 +5,27 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      manrope: ['Manrope', 'sans-serif'],
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        'primaryBlue': '#0085FE',
+        'secondaryBlue': '#000032',
+        'themeBlack': '#0f0f0f',
+        'primaryGrey': '#B0B5B9',
+        'secondaryGrey': '#191919',
+        'backgroundGrey': '#f1f1f1',
+        'cardGrey': '#e4e4e4',
+        'ethereum': '#627EEA',
+        'optimism': '#FF0420',
+        'base': '#FBBB00',
+        'polygon': '#8248D8',
+        'arbitrum': '#00012C',
+      }
     },
   },
   plugins: [],
